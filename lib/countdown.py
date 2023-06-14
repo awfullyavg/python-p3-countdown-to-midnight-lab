d6998b69e9e180f00c8c39b41590c5c0e36877d9
Bryant Wiggins <topwigit@gmail.com>
2023-06-14 08:51:52 -0400

    working

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+def countdown(num):
+    while num:
+        print(f'{num} SECOND(S)!')
+        num -= 1   
+    print('HAPPY NEW YEAR!')
+
+
+def countdown_with_sleep(num):
+    while num:
+        print(f'{num} SECOND(S)!')
+        time.sleep(1)
+        num -= 1
+    print('HAPPY NEW YEAR!')
+
+countdown_with_sleep(10)
